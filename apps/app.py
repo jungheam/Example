ce6b1bf4e1ed9be684b334f3e497d09624856a2f
ahe12@naver.com <ahe12@naver.com>
2024-04-25 17:12:35 +0900

    사용자화면 완료

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,28 @@
+from pathlib import Path
 from flask import Flask
-def create_app():
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from apps.config import config
+
+db =SQLAlchemy()
+csrf = CSRFProtect()
+
+def create_app(config_key):
     app = Flask(__name__)
+    app.config.from_object(config[config_key])
+    # app.config.from_mapping(
+    #     SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+    #     SQLALCHEMY_DATABASE_URI=
+    #         f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #     SQLALCHEMY_ECHO=True,
+    #     WTF_CSRF_SECRET_KEY="AUwzyszU5sugKN7KZs6f"
+    # )
+
+    csrf.init_app(app)
+    db.init_app(app)
+    Migrate(app, db)
 
     from apps.crud import views as crud_views
     
